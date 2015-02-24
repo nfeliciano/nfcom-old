7ec760733229d7362537690d7aa106c58c4d341e
nfeliciano <feliciano.noel@gmail.com>
2015-02-24 02:21:22 -0800

    new fonts, new temp picture

--- client/js/controllers/aboutController.js
+++ client/js/controllers/aboutController.js
@@ -1,3 +1,19 @@
 app.controller('aboutController', ['$scope', function($scope) {
-    $scope.message = "about here";
+    $scope.aboutOne = "I like developing apps on web and mobile. I love working with and talking tech and games and how they can be leveraged to improve personal lives, collaboration, and education. I'm currently a student, and forever a learner. I'm from Manila, The Philippines but based in BC, Canada.";
+
+    $scope.majorSkills = [
+        'iOS & Objective-C',
+        'Javascript (AngularJS, Node.js)',
+        'Git & GitHub',
+        'HTML & CSS',
+        'Bootstrap'
+    ];
+
+    $scope.minorSkills = [
+        'MongoDB',
+        'Python',
+        'Wordpress',
+        'Java',
+        'C'
+    ];
 }]);
