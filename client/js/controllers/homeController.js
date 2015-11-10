f89039e47fd2b7881d1e45ef6fe2d1c6f787f407
Noel Feliciano <feliciano.noel@gmail.com>
2015-11-09 17:09:41 -0800

    about me and some temp images

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -2,7 +2,9 @@ nfcom.controller('homeCtrl', ['$scope', function($scope)  {
     $scope.title = "Noel Feliciano";
     $scope.jobTitle = "Software Developer";
 
-    $scope.message = "Welcome! Here, you'll find everything to know about me, from the iOS and Web Apps I work on to the things I'm enjoying learning about to even my hobbies and interests."
+    // $scope.intro = "Welcome! Thanks for visiting. This site Here, you'll find everything to know about me, from the iOS and Web Apps I work on to the things I'm enjoying learning about to even my hobbies and interests."
 
-    $scope.messageTwo = "I enjoy working with and talking tech and games, and how they can be leveraged to improve people's lives. Everytime I solve a coding problem, especially when I'm working with others to solve it, I fall more and more in love with coding. I hope to work in educational technology, mobile or web application development, games, or an intersection of the three. Living in beautiful Victoria, BC, I graduated with an MASc in Computer Science from UVic, and while I'm no longer a student, I will always be a learner.";
+    $scope.intro = "Welcome! Thanks for stumbling in. This site is my:"
+
+    $scope.messageTwo = "I enjoy working with and talking tech and games, and how they can be leveraged to improve people's lives. Everytime I solve a coding problem, especially when I'm working with others to solve it, I fall more and more in love with coding. I hope to work in educational technology, mobile or web application development, games, or an intersection of the three. Living in beautiful Victoria, BC, I graduated with an MSc in Computer Science from UVic, and while I'm no longer a student, I will always be a learner.";
 }]);
