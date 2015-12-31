383b216e013f091912f16d1f8b2d25e16abc86da
Noel Feliciano <feliciano.noel@gmail.com>
2015-12-31 00:24:05 -0800

    getting projects going

--- client/js/controllers/menuController.js
+++ client/js/controllers/menuController.js
@@ -1,7 +1,20 @@
 nfcom.controller('menuCtrl', ['$scope', '$location', function($scope, $location) {
+	$scope.menuItems = [
+		{ name: 'projects' },
+		{ name: 'home' },
+		{ name: 'blog' }
+	];
+
+	$scope.selected = $location.path().slice(1);
+	if($scope.selected.length <=1) $scope.selected = 'home';
+
 	$scope.twitterLink = "https://twitter.com/nfeli";
 	$scope.githubLink = "https://github.com/nfeliciano";
 	$scope.facebookLink = "https://www.facebook.com/noelfeli";
 	$scope.linkedinLink = "https://ca.linkedin.com/pub/noel-joseph-feliciano/80/29/a67";
 	$scope.mailLink = "mailto:feliciano.noel@gmail.com";
+
+	$scope.changePage = function(item) {
+		$scope.selected = item;
+	}
 }]);
--- client/js/services/projectService.js
+++ client/js/services/projectService.js
@@ -1,16 +1,38 @@
 projectsModule.factory('projectFactory', function() {
-    var projectOne = {  name: 'LFI 2.0',
-                        images: null,
-                        dates: '2012-2015',
-                        link: 'http://web.uvic.ca/~carte',
-                        tasks: 'Everything',
-                        lessons: 'Leadership, iOS Dev'  }
-    var projectTwo = {  name: 'LinkSupp',
-                        images: null,
-                        dates: '2014-2015',
-                        link: 'http://www.linksupp.com',
-                        tasks: 'Backend, etc.',
-                        lessons: 'Meeting a deadline, javascript'  }
+    var projectOne = {
+            name: 'LFI 2.0',
+            cover: '/img/screenshots/lfi/1.jpg',
+            images: [],
+            video: null,
+            what: 'An iPad app targeted towards children with autism.',
+            when: '2012-2015',
+            where: 'Centre for Autism Research, Technology, and Education.',
+            who: 'Started as a developer on the tech team, now leading the team getting ready for app release',
+            link: 'http://web.uvic.ca/~carte',
+            tasks: 'Everything',
+            lessons: 'Leadership, iOS Dev',
+            tech: 'iOS, objective-c, core data'
+                    }
+    var projectTwo = {
+            name: 'LinkSupp',
+            cover: '/img/screenshots/linksupp/1.png',
+            images: null,
+            video: null,
+            what: 'A web app to help people find lunch buddies.',
+            when: '2014-2015',
+            where: 'Started as a course project, now just a spare-time project.',
+            who: 'First in a group of 5 in a course, now collaborating with just two others.',
+            link: 'http://www.linksupp.com',
+            tasks: 'Backend, etc.',
+            lessons: 'Meeting a deadline, javascript',
+            tech: 'Javascript, MEAN, AngularJS, MongoDB, NoSQL'
+        }
+    var projectThree = {
+            name: 'JetPackLaserCat',
+            cover: '/img/screenshots/jplc/1.jpg',
+            images: null
+
+    }
     var projectsList = [projectOne, projectTwo];
 
     var projectFactory = {};
